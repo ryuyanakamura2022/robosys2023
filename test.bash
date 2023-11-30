fe9ee0efea33b3d42922070167998421b0e34ee6
Ryuya Nakamura <ryuya8nakamura@gmail.com>
2023-12-01 02:21:46 +0900

    update

--- test.bash
+++ test.bash
@@ -1,75 +1,21 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2023 Ryuya Nakamura 
 # SPDX-License-Identifier: BSD-3-Clause
-　
 ng () {
       echo NG at Line $1
       res=1
 }
-　
 res=0
-　
+
 ### I/O TEST ###
 out=$(seq 2 | ./plus)
-[ "${out}" = "3 -3 2 0.5" ] || ng ${LINENO}
-
+[ "${out}" = '3 -3 2 0.5' ] || ng ${LINENO}
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
+out=$(echo | ./plus) 
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo A | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo a | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo ./plus | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo hogehogehoge | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo 1 + 1 | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo '#' | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo { } | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK 
 exit $res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
